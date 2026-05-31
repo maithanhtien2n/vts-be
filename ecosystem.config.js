6135e08962613777db07244b0320f9b02ad678ce
Tien Thanh <tienmt@imt-soft.com>
2026-05-31 09:25:17 +0700

    update code

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "meekula-be",
+      script: "./app.js",
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+        MONGODB_URI:
+          "mongodb://admin:tn26052024@110.172.28.201:27017/vts-db?authSource=admin",
+        PORT: 31526,
+      },
+    },
+  ],
+};
