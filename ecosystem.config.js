ad6d65c089bb9a99afda920864d889c9d8edb003
amnadsame <68522709+amnadsame@users.noreply.github.com>
2026-06-03 16:25:23 +0700

    + ads

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,12 @@ module.exports = {
           "mongodb://admin:tn26052024@110.172.28.201:27017/vts-db?authSource=admin",
         PORT: 31526,
       },
+      env_production: {
+        NODE_ENV: "production",
+        MONGODB_URI:
+          "mongodb://admin:tn26052024@110.172.28.201:27017/vts-db?authSource=admin",
+        PORT: 31526,
+      },
     },
   ],
 };
