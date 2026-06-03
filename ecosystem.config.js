0877790553b8aacabcd056e4615b38fe6ee3bc09
amnadsame <68522709+amnadsame@users.noreply.github.com>
2026-06-03 16:29:51 +0700

    + ads

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,6 +15,10 @@ module.exports = {
         MONGODB_URI:
           "mongodb://admin:tn26052024@110.172.28.201:27017/vts-db?authSource=admin",
         PORT: 31526,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+        GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
+
       },
     },
   ],
