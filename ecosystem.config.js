3dbe450e5a6315322b636fd2e8a4c7dd32715a5c
Tien Thanh <tienmt@imt-soft.com>
2026-06-05 20:16:38 +0700

    update code

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
         PORT: process.env.PORT || 31526,
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-        GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
+        GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL_PRO,
       },
     },
   ],
